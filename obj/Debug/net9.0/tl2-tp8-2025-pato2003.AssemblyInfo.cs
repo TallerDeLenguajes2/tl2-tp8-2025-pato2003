0eeabdf5eec28e47c0177ef00b966fb474e5faec
pato2003 <patogabrielferreyra@gmail.com>
2025-12-29 21:43:00 -0300

    cambios

--- obj/Debug/net9.0/tl2-tp8-2025-pato2003.AssemblyInfo.cs
+++ obj/Debug/net9.0/tl2-tp8-2025-pato2003.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp8-2025-pato2003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b6f40d8de8c639dc3e87a91b4360fe431c48dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749a576c716780efa25fd4af81d651756f0f7c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp8-2025-pato2003")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp8-2025-pato2003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
